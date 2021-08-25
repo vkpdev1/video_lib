fc8b9b2e2bc0527bc9da8bfeac45518d7cd1cdd7
unknown <vipin@cognetry.com>
2021-08-25 18:43:30 +0530

    Updates

--- custom-service-worker.js
+++ custom-service-worker.js
@@ -26,11 +26,6 @@ const PRECACHE_URLS = [
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
 });
 
 // The activate handler takes care of cleaning up old caches.
